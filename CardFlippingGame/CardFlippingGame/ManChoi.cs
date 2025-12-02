a0a4b090059688bc411fffdf567f56579ab9eb51
lhnhat2005-beep <lhnhat2005@gmail.com>
2025-12-02 17:01:14 +0700

    Add files via upload

---
+++ CardFlippingGame/CardFlippingGame/ManChoi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardFlippingGame
+{
+    public partial class ManChoi : Form
+    {
+        public ManChoi()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+            GameUI UI = new GameUI();
+            this.Resize += UI.f_resize;
+            UI.BackGroundImage(this,"Default");
+            UI.ThemeSelectionButton(this);
+        }
+
+    }
+}
