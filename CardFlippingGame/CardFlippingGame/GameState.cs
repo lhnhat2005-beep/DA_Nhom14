452d804d80524bdfdc0ef71f4b56f73815e3fa56
lhnhat2005-beep <lhnhat2005@gmail.com>
2025-12-15 19:37:00 +0700

    Add files via upload

---
+++ CardFlippingGame/CardFlippingGame/GameState.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardFlippingGame
+{
+    [Serializable]
+    internal class GameState
+    {
+        [Serializable]
+        public class CardData
+        {
+            public string Picture{get;set;}
+            public bool Isflipped {get;set;}
+            public bool isMatched { get;set;}
+        }
+        public int SoDongBang { get;set;}
+        public int SoCotBang {  get;set;}
+        public int SoCoHoi {  get;set;}
+
+        public List<CardData> CardDataList;
+    }
+}
---
+++ CardFlippingGame/CardFlippingGame/ManChoi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CardFlippingGame
+{
+    public partial class ManChoi : Form
+    {
+        public ManChoi()
+        {
+            InitializeComponent();
+            this.BackColor = Color.Black;
+            GameLogic gameLogic = new GameLogic();
+            GameManage gameManage=new GameManage();
+            gameManage.Chinh();
+            GameUI UI = new GameUI(gameManage,gameLogic);
+            this.Resize += UI.f_resize;
+            UI.BackGroundImage(this,"Default");
+            UI.ThemeSelectionButton(this);
+        }
+
+    }
+}
